1192c620a37bb1b0223c331395401591f199eda0
Phuc Phan <phucpt@Phuc-Phan.local>
2024-02-02 23:09:55 +1100

    88. Merge Sorted Array (formated)

--- java_solution/MergeSortedArray.java
+++ java_solution/MergeSortedArray.java
@@ -3,7 +3,7 @@ package java_solution;
 public class MergeSortedArray {
 
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int[] res = new int[m+n];
+    int[] res = new int[m + n];
     int i = 0, j = 0;
     int cur = 0;
     while (i < m || j < n) {
@@ -23,14 +23,8 @@ public class MergeSortedArray {
       ++cur;
     }
     i = 0;
-    for (; i < m+n; ++i) {
+    for (; i < m + n; ++i) {
       nums1[i] = res[i];
     }
-  }
-
-  public static void main(String[] args) {
-    new MergeSortedArray().merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
-
-    return;
   }
 }
