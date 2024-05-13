8ab94ca993043128f7ddfc2c24804c6537ac6e14
Phuc Phan <phucpt@Phuc-Phan.local>
2024-05-13 21:17:39 +1000

    300. Longest Increasing Subsequence (nlogn)

--- java_solution/LongestIncreasingSubsequence.java
+++ java_solution/LongestIncreasingSubsequence.java
@@ -1,9 +1,13 @@
 package java_solution;
 
-// Time complexity: O(n^2)
-// Space complexity: O(n)
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LongestIncreasingSubsequence {
 
+  // Time complexity: O(n^2)
+  // Space complexity: O(n)
   public int lengthOfLIS(int[] nums) {
     int[] l = new int[nums.length];
     int res = 1;
@@ -21,5 +25,28 @@ public class LongestIncreasingSubsequence {
     }
 
     return res;
+  }
+
+  // Time complexity: O(n*logn)
+  // Space complexity: O(n)
+  public int lengthOfLIS2(int[] nums) {
+    int length = nums.length;
+    List<Integer> ans = new ArrayList<>();
+
+    ans.add(nums[0]);
+
+    for (int i = 0; i < length; ++i) {
+      if (nums[i] > ans.get(ans.size() - 1)) {
+        ans.add(nums[i]);
+      } else {
+        int low = Collections.binarySearch(ans, nums[i]);
+        if (low < 0) {
+          low = - (low + 1);
+        }
+        ans.set(low, nums[i]);
+      }
+    }
+
+    return ans.size();
   }
 }
