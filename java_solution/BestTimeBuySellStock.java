505fdbe5783f142e6819a6ef617714f7e54ecb18
Phuc Phan <phucpt@Phuc-Phan.local>
2024-05-31 10:57:25 +1000

    122. Best Time to Buy and Sell Stock II

--- java_solution/BestTimeBuySellStock.java
+++ java_solution/BestTimeBuySellStock.java
@@ -15,4 +15,20 @@ public class BestTimeBuySellStock {
 
     return res;
   }
+
+  // Time complexity: O(n)
+  // Space complexity: O(1)
+  public int maxProfit2(int[] prices) {
+    int res = 0;
+    int currentBuy = prices[0];
+
+    for (int i = 1; i < prices.length; ++i) {
+      if (prices[i] > currentBuy) {
+        res += prices[i] - currentBuy;
+      }
+      currentBuy = prices[i];
+    }
+
+    return res;
+  }
 }
